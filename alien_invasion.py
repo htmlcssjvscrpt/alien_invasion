79bdbc16dbd1c159b9ec1ee644378368c259ef5f
Minh Frances <131257637+htmlcssjvscrpt@users.noreply.github.com>
2023-07-21 12:36:28 +0800

    Add code to move ship left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
@@ -19,20 +20,41 @@ class AlienInvasion:
         # Set the background color.
         self.bg_color = (230, 230, 230)
 
+        self.ship = Ship(self);
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)    
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
+    
+    def _check_events(self):
+        # Respond to keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        """Update images on the scren, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)    
+        self.ship.blitme()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
